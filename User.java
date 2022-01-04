786f20f5155fd15ec357dd613dc8b207138ed20f
LauraMViana <90008021+LauraMViana@users.noreply.github.com>
2022-01-04 00:18:06 -0300

    file certo

---
+++ User.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String senha;
+	
+	@Column(nullable = false, length = 20)
+	private String Nome;
+	
+	@Column(nullable = false, length = 20)
+	private String sobreNome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
